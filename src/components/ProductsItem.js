de9af867bc5b02a9f61f74af4324a27a0d9e0045
neocull <34644499+neocull@users.noreply.github.com>
2019-07-04 15:53:24 +0400

    Update ProductsItem.js

--- src/components/ProductsItem.js
+++ src/components/ProductsItem.js
@@ -3,17 +3,14 @@ import React from 'react'
 class ProductsItem extends React.Component {
 
     render() {
-        const { name, price, quantity } = this.props.items;
+        const { id, name, price, quantity } = this.props.items;
         return (
             <tr>
                 <td className="article__name">{name}</td>
                 <td className="article__price">{price}</td>
                 <td className="article__quantity">{quantity}</td>
                 <td className="article__buttons">
-                    <button onClick={(e) => {
-                        console.log(e.target);
-                        return this.props.addQuantity(e);
-                    }}
+                    <button onClick={() => this.props.addQuantity(id)}
                                         className="article__buttons_minus">+</button>
                 </td>
             </tr>
