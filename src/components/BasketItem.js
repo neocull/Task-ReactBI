51b46fa8933539842cd72c57348298a525f8cb92
neocull <34644499+neocull@users.noreply.github.com>
2019-07-04 15:52:45 +0400

    Update BasketItem.js

--- src/components/BasketItem.js
+++ src/components/BasketItem.js
@@ -7,17 +7,17 @@ class BasketItem extends React.Component {
     }
 
     render() {
-        const { name, price, quantity } = this.props.items;
+        const { id, name, price, quantity } = this.props.items;
         return (
             <tr>
                 <td className="article__name">{name}</td>
                 <td className="article__price">{this.sumPrice(price, quantity)}</td>
                 <td className="article__quantity">{quantity}</td>
                 <td className="article__buttons">
-                    <button onClick={(e) => this.delQuantity(e)}
+                    <button onClick={() => this.props.delQuantity(id)}
                             className="article__buttons_minus">-
                     </button>
-                    <button onClick={(e) => this.delAllQuantity(e)}
+                    <button onClick={() => this.props.delAllQuantity(id)}
                             className="article__buttons_del">Удалить все
                     </button>
                 </td>
